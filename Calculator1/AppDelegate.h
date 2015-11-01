d20acb9041d0cf6ffbfcf156ff661706a3d208f7
leotalero <leo.20waw@hotmail.com>
2015-11-01 16:44:27 -0500

    Initial Commit

---
+++ Calculator1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator1
+//
+//  Created by Leonardo Talero on 11/1/15.
+//  Copyright © 2015 unir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator1
+//
+//  Created by Leonardo Talero on 11/1/15.
+//  Copyright © 2015 unir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
